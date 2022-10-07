122829270b39c357a86379f18cdffa9de1ec4df2
imad3100 <95374795+imad3100@users.noreply.github.com>
2022-10-07 15:31:57 +0100

    Add files via upload

---
+++ React Budget Tracker -/src/components/ExpenseForm.js
@@ -0,0 +1,37 @@
+import React, { useRef } from 'react'
+import { useDispatch } from 'react-redux'
+import { addExpense } from '../features/expenseSlice'
+ 
+const ExpenseForm = () => {
+  const dispatch=useDispatch()
+ const nameRef = useRef(null)
+ const costRef = useRef()  
+
+ function submit() {
+    
+  dispatch(addExpense({name:nameRef.current.value,cost:costRef.current.value}))
+
+ }
+   
+  return (<div className='mt-5' >
+    <h2 className='d-block'>Add Expense</h2>
+    <form className='d-flex mt-3 '>
+     
+  <div class="mb-3 col-sm-4 ">
+    <label for="exampleInputEmail1" class="form-label "> Name</label>
+    <input type="text" ref={nameRef} class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+   
+  </div>
+  <div class="mx-2 col-sm-4">
+    <label for="exampleInputPassword1" class="form-label">Cost</label>
+    <input type="text" ref={costRef} class="form-control" id="exampleInputPassword1"/>
+  </div>
+  <div class="mx-2 col-sm-4 submit "  >
+  <button onClick={(e)=>{ e.preventDefault();submit()}} type="submit" class="btn btn-primary">Submit</button>
+  </div>
+</form>
+  
+  </div>)
+}
+
+export default ExpenseForm
