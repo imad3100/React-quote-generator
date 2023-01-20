f1b37639b3a473c32759b71a8b93f8df5165107e
imad3100 <95374795+imad3100@users.noreply.github.com>
2023-01-20 17:58:41 +0100

    Add files via upload

---
+++ React Quiz app -/src/questions.js
@@ -0,0 +1,74 @@
+export default  [  {    question: "What is the primary function of a computer's CPU?",    options: [      "To manage and coordinate the activities of a computer",      "To perform complex calculations and execute instructions",      "To ensure network security",      "To run software applications"    ],
+correctAnswer: "To perform complex calculations and execute instructions"
+},
+{
+question: "What is the full form of RAM?",
+options: [
+  "Random Access Memory",
+  "Random Array Memory",
+  "Random Access Module",
+  "Random Array Module"
+],
+correctAnswer: "Random Access Memory"
+},
+{
+question: "Which of the following is not a programming language?",
+options: [
+  "Java",
+  "Python",
+  "HTML",
+  "Word"
+],
+correctAnswer: "Word"
+},
+{
+question: "Which company developed the JavaScript programming language?",
+options: [
+  "Microsoft",
+  "Oracle",
+  "Netscape",
+  "Apple"
+],
+correctAnswer: "Netscape"
+},
+{
+question: "Which of the following is not a database management system?",
+options: [
+  "MySQL",
+  "PostgreSQL",
+  "MongoDB",
+  "Microsoft Word"
+],
+correctAnswer: "Microsoft Word"
+},
+{
+question: "Which of the following is not a version control system?",
+options: [
+  "Git",
+  "SVN",
+  "Mercurial",
+  "Dropbox"
+],
+correctAnswer: "Dropbox"
+},
+{
+question: "Which of the following is not a web framework?",
+options: [
+  "React",
+  "Vue",
+  "Angular",
+  "Microsoft Excel"
+],
+correctAnswer: "Microsoft Excel"
+},
+{
+question: "Which of the following is not a web browser?",
+options: [
+  "Google Chrome",
+  "Mozilla Firefox",
+  "Apple Safari",
+  "Microsoft PowerPoint"
+],
+correctAnswer: "Microsoft PowerPoint"
+}
+];
