ef6a990368319b88fb2363735ba4279353aa9d21
imad <imadmath99@gmail.com>
2023-06-21 01:59:44 +0100

    Your commit message

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -28,14 +28,14 @@ function Quote() {
   return (
     <div className="container">
       <div className="row mt-5">
-        <div className="col-lg-6 mx-auto">
+        <div className="col-lg-6 col-11 mx-auto">
           <blockquote className="blockquote blockquote-custom p-5 shadow rounded">
             <div className="blockquote-custom-icon bg-info shadow-sm">
               <i className="fa fa-quote-left text-white"></i>
             </div>
-            <p className="mb-0 mt-2 font-italic display-6">{quote.text}</p>
+            <p className="mb-0 mt-2 bolder-font font-italic display-6">{quote.text}</p>
             <footer className="blockquote-footer pt-4 mt-4 border-top">
-              {quote.author}
+              {quote.author ? quote.author : 'Unknown'}
             </footer>
             <button
               type="button"
